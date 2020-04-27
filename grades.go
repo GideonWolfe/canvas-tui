261cddb66868166df71e4ec4ebc78b83734ed153
GideonWolfe <wolfeg2@wwu.edu>
2020-04-26 21:33:13 -0700

    hacky version of grades by assignment group

--- grades.go
+++ grades.go
@@ -63,50 +63,43 @@ func createGradeTable(assignments *[]Assignment) *widgets.Table {
   return gradeTable
 }
 
-func createAGBreakdown(assignments *[]Assignment) *widgets.Table {
+func createAGBreakdown(assignments *[]Assignment, assignmentGroups *[]AssignmentGroup) *widgets.Plot {
 
-  gradeTable := widgets.NewTable()
-  var tableData [][]string
-  header := []string{"Name",  "Score", "%"}
-  i := 1
+  p0 := widgets.NewPlot()
+	p0.Title = "Score by Assignment Group"
+  var dataLabels []int
+
+  // create a dictionary of assignment groups and their IDs
+  count := 0
+  aGDict := make(map[int]string)
+  for _, ag := range *assignmentGroups {
+    if len(ag.Assignments) > 0 {
+      count++
+      aGDict[ag.ID] = ag.Name
+    }
+  }
+
+  p0.Data = make([][]float64, count)
+  assignmentDict := make(map[int][]float64)
+
   for _, assn := range *assignments {
     if !assn.Submission.SubmittedAt.IsZero() {
-      if assn.Submission.Score > 0{
-        var assignmentData []string
-        assignmentData = append(assignmentData, assn.Name)
+      if assn.Submission.Score > 0 && assn.PointsPossible != 0{
         percentScored := float64(assn.Submission.Score/assn.PointsPossible)*100
-        scoreString := fmt.Sprint(assn.Submission.EnteredScore)+"/"+fmt.Sprint(assn.PointsPossible)
-        assignmentData = append(assignmentData, scoreString)
-        assignmentData = append(assignmentData, fmt.Sprintf("%.f%%", percentScored))
-        tableData = append(tableData, assignmentData)
-        if percentScored > 90 {
-          gradeTable.RowStyles[i-1] = ui.NewStyle(ui.ColorGreen, ui.ColorClear, ui.ModifierBold)
-        } else if percentScored > 80 {
-          gradeTable.RowStyles[i-1] = ui.NewStyle(ui.ColorBlue, ui.ColorClear, ui.ModifierBold)
-        } else if percentScored > 70 {
-          gradeTable.RowStyles[i-1] = ui.NewStyle(ui.ColorYellow, ui.ColorClear, ui.ModifierBold)
-        } else if percentScored <= 60 {
-          // log.Panic(assn.Name)
-          gradeTable.RowStyles[i-1] = ui.NewStyle(ui.ColorRed, ui.ColorClear, ui.ModifierBold)
-        }
-        i++
+        assignmentDict[assn.AssignmentGroupID] = append(assignmentDict[assn.AssignmentGroupID], percentScored)
       }
     }
   }
-  tableData = append(tableData, header)
-  // reverse the list
-  for i, j := 0, len(tableData)-1; i < j; i, j = i+1, j-1 {
-    tableData[i], tableData[j] = tableData[j], tableData[i]
-    gradeTable.RowStyles[i], gradeTable.RowStyles[j] = gradeTable.RowStyles[j], gradeTable.RowStyles[i]
+
+  i := 0
+  for group, data := range assignmentDict {
+    dataLabels = append(dataLabels, group)
+    p0.Data = append(p0.Data, data)
+    i++
   }
 
-  gradeTable.Title = "Scores:"
-  gradeTable.Rows = tableData
-	gradeTable.TextStyle = ui.NewStyle(ui.ColorWhite)
-	gradeTable.RowSeparator = true
-  gradeTable.FillRow = true
-  gradeTable.RowStyles[0] = ui.NewStyle(ui.ColorWhite, ui.ColorBlack, ui.ModifierBold)
-  return gradeTable
+  // log.Panic(p0.Data, assignmentDict)
+  return p0
 }
 
 func createGradeGrid(course Course, assignments *[]Assignment, assignmentGroups *[]AssignmentGroup) *ui.Grid {
@@ -118,7 +111,8 @@ func createGradeGrid(course Course, assignments *[]Assignment, assignmentGroups 
   gradeGrid.Title = "Course Overview Grid"
   gradeGrid.Set(
 		ui.NewRow(1.0, 
-			ui.NewCol(1.0, createGradeTable(assignments)), 
+			ui.NewRow(2.0/3, createGradeTable(assignments)), 
+			ui.NewRow(1.0/3, createAGBreakdown(assignments, assignmentGroups)), 
 		),
   )
 
