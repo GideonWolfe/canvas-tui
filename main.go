337ba0b2378c338cadd68abc47afea55f3826452
GideonWolfe <wolfeg2@wwu.edu>
2020-04-27 10:02:28 -0700

    viper now sees configs in the right directory

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func readConfig() {
   viper.SetConfigName("config") // name of config file (without extension)
   viper.SetConfigType("yaml") // REQUIRED if the config file does not have the extension in the name
   viper.AddConfigPath(".")               // optionally look for config in the working directory
-  // viper.AddConfigPath("$HOME/.config/canvas-tui/")              
+  viper.AddConfigPath("$HOME/.config/canvas-tui/")              
   err := viper.ReadInConfig() // Find and read the config file
   if err != nil { // Handle errors reading the config file
     panic(fmt.Errorf("Fatal error config file: %s \n", err))
